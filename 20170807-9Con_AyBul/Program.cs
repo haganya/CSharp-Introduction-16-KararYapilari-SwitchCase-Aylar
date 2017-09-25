da22ac75da6749a8322e4231d075b030791d0f82
Hakan Akgül <hakanakgul34@gmail.com>
2017-09-25 22:07:00 +0300

    created at 07.08.2017 (dd.mm.yyyy)

---
+++ 20170807-9Con_AyBul/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20170807_9Con_AyBul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // kullanıcının girdiği ay sayılarına bakarak Ocak , Şubat şekilde değer döndüren uygulamayı yazınız.
+
+            Console.Write("Ay giriniz : ");
+            string ay = Console.ReadLine();
+            switch (ay)
+            {
+                case "1": Console.Write("Ocak"); break;
+                case "2": Console.Write("Şubat"); break;
+                case "3": Console.Write("Mart"); break;
+                case "4": Console.Write("Nisan"); break;
+                case "5": Console.Write("Mayıs"); break;
+                case "6": Console.Write("Haziran"); break;
+                case "7": Console.Write("Temmuz"); break;
+                case "8": Console.Write("Ağustos"); break;
+                case "9": Console.Write("Eylül"); break;
+                case "10": Console.Write("Ekim"); break;
+                case "11": Console.Write("Kasım"); break;
+                case "12": Console.Write("Aralık"); break;
+
+                default: Console.Write("öyle bir ay yok."); break;
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ 20170807-9Con_AyBul/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("20170807-9Con_AyBul")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("20170807-9Con_AyBul")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8bc7e410-62db-4128-8fea-7f614a4a7243")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
